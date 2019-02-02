01d7218d5d688712e5754d6fe950969e514d56d7
sherruh <sherruh@gmail.com>
2019-02-02 22:28:53 +0600

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private String model;
+    private String mark;
+    private int vinCode;
+
+    public Car(String model, String mark, int vinCode) {
+        this.model = model;
+        this.mark = mark;
+        this.vinCode = vinCode;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public int getVinCode() {
+        return vinCode;
+    }
+
+    public void setVinCode(int vinCode) {
+        this.vinCode = vinCode;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void checkClass(Class myClass){
+        
+    }
+}
